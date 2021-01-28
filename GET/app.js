1f841acfbaeb1a6eabf02fac36a7192563d1758d
Berke Karataş <uberke.karatas@gmail.com>
2021-01-29 02:12:35 +0300

    get request callback 2parameters status control

--- GET/app.js
+++ GET/app.js
@@ -1,5 +1,3 @@
-
-
 class Request {
     constructor(){
         this.xhr = new XMLHttpRequest();
@@ -7,56 +5,18 @@ class Request {
 
     // GET REQUEST
 
-    get(url){
-        // bağlantımızı açıyoruz
+    get(url,callback){
         this.xhr.open("GET",url);
         
-        /* ---- İLKEL YÖNTEM ---- */
-        // this.xhr.status de hata almamak için, this ile Requesti göstermek için;
-        // geçici olarak bu Requesti yani thisi bir temp değerine atayalım
-        // const temp = this;
-        
-        // response başarılı gelirse çalışıyor
-
-        /* ---- ARROW FUNCTION YÖNTEMİ ---- */
         this.xhr.onload = () => {
-            // bind(this) YÖNTEMİNDEN SONRA;
-            // console.log(this) --> Request'i gösterir
-
-            // buradaki this anahtar kelimesi xhr'ı gösteriyor
-            // console.log(this);
+            // callback 2 parametreli olacak statusun başarısız olma ihtimaline karşı
             if(this.xhr.status === 200){
-                console.log(this.xhr.responseText);
+                callback(null,this.xhr.responseText); 
+            }else {
+                // status 200 değilse hata durumundaysa
+                callback("Hata oluştu",null);
             }
-
-
-
-
-            /* ---- EN BASİT YÖNTEM ---- */
-            // this.xhr.status ile status undefined hatası aldık
-            // this --> xhr gösterdiği için bu şekilde kullanarak hata almayız 
-            /* 
-            if(this.status === 200){
-                console.log(this.responseText);
-            } 
-            */
-
-
-
-
-            /* 
-            ---- İLKEL YÖNTEM ---- 
-            if(temp.xhr.status === 200){
-                console.log(temp.xhr.responseText);
-            } 
-            */
         }
-        /* ---- .bind(this) YÖNTEMİ ---- */
-        //.bind(this)
-        // get fonksiyonda kullanılan this neyse onu kullan diyoruz
-        // get fonksiyonunun kapsama alanında this Requesti gösteriyor
-
-
 
         this.xhr.send();
     }
@@ -64,4 +24,12 @@ class Request {
 }
 
 const request = new Request();
-request.get("https://jsonplaceholder.typicode.com/albums");
+request.get("https://jsonplaceholder.typicode.com/albums",function(err,response){
+    if(err === null){
+        // status başarılı 200
+        console.log(response);
+    }else {
+        // status başarısız - Hata
+        console.log(err);
+    }
+});
